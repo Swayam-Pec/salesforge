609f53af8a38854d48d97f205c86dd8f6b45e073
Rajat Kumar <rajatkumar7861813@gmail.com>
2026-04-28 16:48:33 +0530

    backend: express server bootstrap

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const app = require("./app");
+const { connectPostgres, prisma } = require("./config/postgres");
+const logger = require("./utils/logger");
+const jobs = require("./jobs/followupJob");
+
+const startServer = async (port = Number(process.env.PORT || 3000)) => {
+  await connectPostgres();
+
+  const server = app.listen(port, () => {
+    if (process.env.NODE_ENV !== "test") {
+      logger.info("server.started", { port, env: process.env.NODE_ENV || "development" });
+    }
+  });
+
+  // Kick off background work after the API is accepting requests.
+  jobs.start();
+
+  const shutdown = async (signal) => {
+    logger.info("server.shutdown", { signal });
+    server.close(async () => {
+      await prisma.$disconnect();
+      process.exit(0);
+    });
+    // Hard kill if shutdown stalls.
+    setTimeout(() => process.exit(1), 10_000).unref();
+  };
+
+  process.once("SIGINT", () => shutdown("SIGINT"));
+  process.once("SIGTERM", () => shutdown("SIGTERM"));
+
+  return server;
+};
+
+if (require.main === module) {
+  startServer().catch((error) => {
+    logger.error("server.failed", { err: error.message });
+    process.exit(1);
+  });
+}
+
+module.exports = { app, startServer };
